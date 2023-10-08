4314db164c1b77a706511834aadca90e82290f62
Bikalpa <bikalpasangat1@gmail.com>
2023-10-08 20:01:34 +0545

    Adding Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
 import './App.css'
-
+import {BrowserRouter, Routes,Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
 function App() {
 
   return (
-    <>
-      <h1 className='heading'>Hello</h1>
-      <h3>I am HAHA</h3>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
