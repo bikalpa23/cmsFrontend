f2495271d640fc65ef4be5dfc4929cf2e87c766e
Bikalpa <bikalpasangat1@gmail.com>
2023-10-11 18:54:13 +0545

    Using Use States

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Contact from './pages/Contact/Contact'
 import Test from './pages/Test/Test'
+import UseEffect from './pages/UseEffect/UseEffect'
+
+
 function App() {
 
   return (
@@ -13,6 +16,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/test' element={<Test/>}/>
+        <Route path='/useEffect' element={<UseEffect/>}/>
       </Routes>
     </BrowserRouter>
   )
